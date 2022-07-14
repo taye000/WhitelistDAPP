f4b37650b84a02216551caa366489d8281dac865
taye000 <taylorgitari@gmail.com>
2022-07-14 17:12:35 +0300

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL;
+const PRIVATEKEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_URL,
+      accounts: [PRIVATEKEY],
+    },
+  },
 };
